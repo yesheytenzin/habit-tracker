5600bc8d8a2e3fde478f2d31a2db713a68f938a2
yeshey tenzin <yesheytenzin0@gmail.om>
2024-11-09 21:54:56 +0600

    9v

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,30 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/theme/light_mode.dart';
 import 'package:habit_tracker/theme/dark_mode.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class ThemeProvider extends ChangeNotifier {
+  late SharedPreferences prefs;
+  late ThemeData _themeData;
 
-class ThemeProvider extends ChangeNotifier{
-  //initially light mode
-  ThemeData _themeData = lightmode;
+  ThemeProvider() {
+    _themeData = lightmode; // Default to light mode
+    _loadThemeFromPrefs(); // Load saved theme
+  }
 
-  //get curren theme
-  ThemeData get themedata =>  _themeData;
-
-  //is dark mode
-  bool get isDarkMode => _themeData == darkmode;
-
-  //set theme
-  set themedata(ThemeData themedata){
-    _themeData = themedata;
+  // Initialize SharedPreferences
+  Future<void> _loadThemeFromPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final bool isDark = prefs.getBool('isDarkMode') ?? false;
+    _themeData = isDark ? darkmode : lightmode;
     notifyListeners();
   }
 
-  //toggle theme
-  void toggleTheme(){
-    if(_themeData == lightmode){
-      themedata = darkmode;
-    }else{
-      themedata = lightmode;
+  // Get theme
+  ThemeData get themedata => _themeData;
+
+  // Check if dark mode
+  bool get isDarkMode => _themeData == darkmode;
+
+  // Toggle theme
+  void toggleTheme() async {
+    if (_themeData == lightmode) {
+      _themeData = darkmode;
+      await prefs.setBool('isDarkMode', true);
+    } else {
+      _themeData = lightmode;
+      await prefs.setBool('isDarkMode', false);
     }
+    notifyListeners();
   }
 }
